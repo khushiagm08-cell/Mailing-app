be45d65ddaf59e02d8b26c78096482d02154e54e
khushiagm08-cell <khushiagm08@gmail.com>
2025-11-12 21:40:19 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js
+export const EMAILJS_CONFIG = {
+  SERVICE_ID: "service_xx7dy4s",
+  TEMPLATE_ID: "template_nsnl7dp",
+  PUBLIC_KEY: "Q-rTY4HCvZClEB8sM"
+};
+
+export const GMAIL_CONFIG = {
+  API_KEY: "your_google_api_key_here",
+  CLIENT_ID: "your_google_client_id_here",
+  SCOPES: "https://www.googleapis.com/auth/gmail.send"
+};
